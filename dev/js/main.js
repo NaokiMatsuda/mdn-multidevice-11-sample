0d9c6274ab09913a5b13c7031923d0202d14a067
松田直樹 <readymadegogo@gmail.com>
2015-10-29 17:06:14 +0900

    update jade template

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,18 @@
 ;(function($){
 
   'use strict';
-  console.log('Sample Dev Site is here.');
+
+  // Smooth Scroll
+  $('a[href^="#"]').smoothScroll({
+    duration : 3000
+  });
+
+  // Bootstrap Auto-Hiding Navbar
+  $(".navbar-fixed-top").autoHidingNavbar();
+
+  $.slidebars();
+
+  // Your Code
+  console.log('Write Your Code.');
 
 })(jQuery);
